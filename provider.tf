ce10162f3ff87240d5295e819ee7c25dfb1d6cf4
prasadallu121 <61218379+prasadallu121@users.noreply.github.com>
2020-09-07 19:20:17 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  version = "~> 3.0"
+  region  = "us-east-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Green-VPC"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Green-SN"
+  }
+}
